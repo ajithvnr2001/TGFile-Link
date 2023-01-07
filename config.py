988d7e6240ab9a7941e3c8568c6c4e474b7a3504
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2023-01-07 18:43:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOTTOKEN = os.environ.get("BOTTOKEN", "")
-APIID = int(os.environ.get("APIID", 6))
-APIHASH = os.environ.get("APIHASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+BOTTOKEN = os.environ.get("BOTTOKEN", "5945791399:AAFXsesW-ra5DrrojpYgVJgMq4lMOeYIiOo")
+APIID = int(os.environ.get("APIID", 1954515))
+APIHASH = os.environ.get("APIHASH", "9ff4a51876a555bdb530cd6a58baae87")
 DOWNLOADPATH = os.environ.get("DOWNLOADPATH", "Downloads/")
-USERNAME = "@URLYUKLEBOT"
+USERNAME = "@filetourlvnr_bot"
 
 """
 If you using VPS, edit like this:
@@ -13,5 +13,5 @@ BOTTOKEN = "123456:xxxx"
 APIID = 6
 APIHASH = eb06d4abfb49dc3eeb1aeb98ae0f581e
 DOWNLOADPATH = "Downloads/"
-USERNAME = "@URLYUKLEBOT"
+USERNAME = "@UR"
 """
